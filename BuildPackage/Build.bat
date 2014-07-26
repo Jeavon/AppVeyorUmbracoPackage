84c57927e2e841bbd164137790556561c59b6e34
Jeavon Leopold <jeavon@crumpled-dog.com>
2014-07-26 15:57:09 +0100

    Restore

--- BuildPackage/Build.bat
+++ BuildPackage/Build.bat
@@ -1,3 +1,2 @@
-Call ..\.nuget\nuget.exe sources add -Name https://www.myget.org/F/umbraco7nightly -Source https://www.myget.org/F/umbraco7nightly -NonInteractive
-Call ..\.nuget\nuget.exe restore ..\AppVeyorUmbracoPackage.sln -source "https://www.nuget.org/api/v2;https://www.myget.org/F/umbraco7nightly"
+Call nuget.exe restore ..\AppVeyorUmbracoPackage.sln
 Call C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\msbuild.exe ..\AppVeyorUmbracoPackage.sln
