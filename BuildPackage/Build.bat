e75dc89172f9315c863fd09d243935f6b7425fca
Jeavon Leopold <jeavon@crumpled-dog.com>
2014-07-28 10:05:53 +0100

    Tweak

--- BuildPackage/Build.bat
+++ BuildPackage/Build.bat
@@ -1,2 +1,3 @@
+ECHO APPVEYOR_REPO_BRANCH: %APPVEYOR_REPO_BRANCH%
 Call nuget.exe restore ..\AppVeyorUmbracoPackage.sln
 Call C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\msbuild.exe Package.build.xml
