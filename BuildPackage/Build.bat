cc0f202526f28b0e68f5464d6b4773f5b540d140
Jeavon Leopold <jeavon@crumpled-dog.com>
2014-08-14 19:29:12 +0100

    Add GruntProject Dist into package

--- BuildPackage/Build.bat
+++ BuildPackage/Build.bat
@@ -1,5 +1,9 @@
 ECHO APPVEYOR_REPO_BRANCH: %APPVEYOR_REPO_BRANCH%
 ECHO APPVEYOR_BUILD_NUMBER : %APPVEYOR_BUILD_NUMBER%
 ECHO APPVEYOR_BUILD_VERSION : %APPVEYOR_BUILD_VERSION%
+cd ..\Angular\
+Call npm install
+Call node -e "require('grunt').tasks(['default']);"
+cd ..\BuildPackage\
 Call nuget.exe restore ..\AppVeyorUmbracoPackage.sln
 Call "C:\Program Files (x86)\MSBuild\12.0\Bin\MsBuild.exe" Package.build.xml
