2dc4ae36da9e4b7ed05e1439c7c3d4d7a4547529
Jeavon Leopold <jeavon@crumpled-dog.com>
2015-01-30 19:31:58 +0000

    Switching to using Github tag for deployment trigger

--- Test/Properties/AssemblyInfo.cs
+++ Test/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -31,7 +31,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.2.2.0")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0-alpha")]
+// [assembly: AssemblyVersion("1.0.0.100")]
+[assembly: AssemblyVersion("1.0.0.100")]
+[assembly: AssemblyFileVersion("1.0.0.100")]
+[assembly: AssemblyInformationalVersion("1.0.0-build100")]
