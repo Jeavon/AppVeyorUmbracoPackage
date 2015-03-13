fa91a171c2bd62072ac92109889cffde6de5fa42
Jeavon Leopold <jeavon@crumpled-dog.com>
2015-03-13 14:47:13 +0000

    New build numbers WIP - Grunt not currently working

--- Test/Class1.cs
+++ Test/Class1.cs
@@ -1,5 +1,7 @@
 ﻿namespace Test
 {
+    using System.Globalization;
+
     public static class Class1
     {
         public static string AMethod()
@@ -7,6 +9,38 @@
             var a = 1;
             var d = 6 * 99;
             return d.ToString();
+        }
+
+        public static string BuildVersion = "1.0.0.14";
+        public static string BuildSuffix = "beta";
+        public static string Release = "false";
+        public static string ProductVersion;
+
+        public static void TestAppVeyor()
+        {
+            var pos = BuildVersion.LastIndexOf('.');
+            var len = BuildVersion.Length - pos - 1;
+            
+            var buildNumber = ("000" + BuildVersion.Substring(pos + 1, len));
+            var buildNumberWithZeros = buildNumber.Substring(buildNumber.Length - 4);
+
+            var baseVersion = BuildVersion.Substring(0, pos);
+
+            if (string.IsNullOrEmpty(BuildSuffix) && Release == "true")
+            {
+                ProductVersion = baseVersion;
+            }
+            else if (string.IsNullOrEmpty(BuildSuffix) && Release == "false")
+            {
+                ProductVersion = baseVersion + "-build-" + buildNumberWithZeros;
+            }
+            else if (!string.IsNullOrEmpty(BuildSuffix) && Release == "true")
+            {
+                ProductVersion = baseVersion + "-" + BuildSuffix;
+            }
+            else if (!string.IsNullOrEmpty(BuildSuffix) && Release == "false"){
+                ProductVersion = baseVersion + "-" + BuildSuffix + "-" + buildNumberWithZeros;
+            }            
         }
     }
 }
---
+++ Test/Test.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    using System.Runtime.InteropServices;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        public void AppVeyor()
+        {
+            Class1.TestAppVeyor();
+            Assert.AreEqual("1.0.0-build-0014", Class1.ProductVersion);
+        }
+    }
+}
